9868740e75e7ee41676e10e0d510c9a9e47d7eff
Muhtasim Ferdous <mferdous98@aol.com>
2020-06-24 01:30:48 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -2,19 +2,4 @@ def mario
   phrase = "It's a-me, Mario!"
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
-
-def toadstool
-  puts status
-end
-end
-
-def link 
-  journey = "It's Dangerous To Go Alone! Take This."
-  puts journey
-
-def all-phrases 
-  puts phrase 
-  puts status
-  puts journey
-end
 end
